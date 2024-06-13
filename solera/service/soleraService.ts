f87f1dd8fffc024ba771901234e1908cda26387c
roman-strilets <roman@beam-mw.com>
2024-06-13 20:38:55 +0200

    implemented simple version of solera service

--- solera/service/soleraService.ts
+++ solera/service/soleraService.ts
@@ -8,30 +8,34 @@ const serviceManagerABI = [
     "function submitTask(uint256 _srcChainId, uint256 _dstChainId, address _sender, address _recipient, uint256 _amount)"
 ];
 const soleraContractABI = [
-    ""
+    "function mint(address to, uint256 amount)"
 ];
 
-const chains = new Map<BigInt, string>([
-    [2n, ""],
-    [3n, ""],
-]);
-
-const contracts = new Map<BigInt, string>([
-    [2n, ""],
-    [3n, ""],
-]);
+let chains = new Map<BigInt, string>();
+let tokenAddresses = new Map<BigInt, string>();
 
 const ethProvider = new ethers.JsonRpcProvider(process.env.ETH_URL);
 const serviceManager = new ethers.Contract(process.env.SERVICE_MANAGER_ADDRESS!, serviceManagerABI, ethProvider);
 
 async function main() {
+    chains.set(BigInt(process.env.CHAIN_ID1!), process.env.URL1!);
+    chains.set(BigInt(process.env.CHAIN_ID2!), process.env.URL2!);
+
+    tokenAddresses.set(BigInt(process.env.CHAIN_ID1!), process.env.TOKEN_ADDRESS1!);
+    tokenAddresses.set(BigInt(process.env.CHAIN_ID2!), process.env.TOKEN_ADDRESS2!);
+    
     serviceManager.on("SubmittedTask", async(srcChainId: BigInt, dstChainId: BigInt, sender: string, recipient: string, amount: BigInt) => {
         console.log("SubmittedTask", srcChainId.toString(), dstChainId.toString(), sender, recipient, amount.toString());
         if (chains.has(dstChainId)) {
             const provider = new ethers.JsonRpcProvider(chains.get(dstChainId));
-            const soleraContract = new ethers.Contract(contracts.get(dstChainId)!, soleraContractABI, ethProvider);
+            const wallet = new ethers.Wallet(process.env.OWNER_SERVICE_KEY!, provider);
+            const tokenContract = new ethers.Contract(tokenAddresses.get(dstChainId)!, soleraContractABI, wallet);
 
-            // TODO call mint token
+            const tx = await tokenContract.mint(recipient, amount.toString());
+
+            await tx.wait();
+
+            console.log("tx hash", tx.hash)
         }
     });
 
