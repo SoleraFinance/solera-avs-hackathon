810531fcc448951dfaf71132526ae852a6912cd0
roman-strilets <roman@beam-mw.com>
2024-06-14 14:33:08 +0200

    added another network to config

--- solera/service/config.ts
+++ solera/service/config.ts
@@ -11,5 +11,9 @@ export const chains = new Map<BigInt, Chain>([
     [137n, {
         url: "http://127.0.0.1:8547",
         tokenAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+    }],
+    [31337n, {
+        url: "https://127.0.0.1:8545",
+        tokenAddress: "0x998abeb3E57409262aE5b751f60747921B33613E"
     }]
 ]);
